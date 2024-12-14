6db69d6f4b540ec04a03fb6357893b756b1acc88
Krzysztof-Polowczyk <Krzysztof.Polowczyk@gmail.com>
2024-12-14 17:51:42 +0100

    endpoints_first_version

--- app/app.py
+++ app/app.py
@@ -9,19 +9,17 @@ app = Flask(__name__)
 
 @app.get("/users")
 def users():
-    return data
+    return data["users"]
 
 @app.get("/users/<id>")
 def addUser(id):
-    return data[int(id)]
+    return data["users"]
 
 @app.post("/users")
 def post_user():
     request.json["id"] = len(data)+1
-    print(request.json)
     data.append(request.json)
-    print(data)
-    return [200, "OK"]
+    return Response(status=200)
 
 @app.patch("/users/<id>")
 def patch_user(id):
@@ -31,9 +29,13 @@ def patch_user(id):
 
 @app.put("/users/<id>")
 def put_user(id):
-    print(id)
-    print(data[int(id)-1][next(iter(request.json))])
-    data[int(id)-1][next(iter(request.json))] = request.json[next(iter(request.json))]
+    request.json["id"] = id
+    data[id-1] = request.json
+    return Response(status=200)
+
+@app.delete("/users/<id>")
+def delate_user(id):
+    
     return Response(status=200)
 
 
